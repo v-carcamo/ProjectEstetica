5a235acf3fe642eef57c5443f6e429c9be13cf2e
Tatiana Vinasco <87791498+tvinasco@users.noreply.github.com>
2022-09-17 11:16:21 -0500

    se optimiza la tabla de productos

--- esteticApp/admin.py
+++ esteticApp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models.cliente import Clientes
+from .models.productos import Productos
 
 
 # Register your models here.
 
 admin.site.register(Clientes)
+admin.site.register(Productos)
--- esteticApp/models/__init__.py
+++ esteticApp/models/__init__.py
@@ -1,1 +1,2 @@
 from .cliente import Clientes
+from .productos import Productos
