97e28b5b354c4f900a2fddb6a003277eec9a04ef
v-carcamo <vcarcamocantillo@gmail.com>
2022-09-17 17:13:44 -0500

    esteticApp

--- esteticApp/models/servicios.py
+++ esteticApp/models/servicios.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Servicios(models.Model):
     id_Servicio=models.BigAutoField(primary_key=True)
-    nombre_Ser=models.CharField('Nombre', max_length=100)
-    precio_Ser=models.CharField('Precio', max_length=100)
-    descripcion_Ser=models.CharField('Descripción', max_length=15)
+    nombre_Ser=models.CharField('Nombre', max_length=50)
+    precio_Ser=models.CharField('Precio', max_length=40)
+    descripcion_Ser=models.CharField('Descripción', max_length=200)
     
